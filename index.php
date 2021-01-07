92dbea5e462b952d2f78538bc5611e7e4e955721
Kausinho <kausinhobrunow@gmail.com>
2021-01-07 20:44:00 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <title>Login Facebook</title>
     <!--Aplicar depois as metas tags-->
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css" />
 </head>
@@ -58,6 +59,53 @@
                     </div><!--w100-->
 
                     <div class="w100">
+                        <h2>Data Nascimento:</h2>
+                        <select name="nascimento-dia" class="nascimento">
+                            <?php
+                                for($i = 1; $i <= 31; $i++){
+                            ?>
+                            <option value="<?php echo $i; ?>"><?php echo $i; ?></option>
+                            <?php } ?>
+                        </select>
+
+                        <select name="nascimento-mes" class="nascimento">  
+                            <option value="1">Janeiro</option>
+                            <option value="2">Fevereiro</option>
+                            <option value="3">Março</option>
+                            <option value="4">Abril</option>
+                            <option value="5">Maio</option>
+                            <option value="6">Junho</option>
+                            <option value="7">Julho</option>
+                            <option value="8">Agosto</option>
+                            <option value="9">Setembro</option>
+                            <option value="10">Outubro</option>
+                            <option value="11">Novembro</option>
+                            <option value="12">Dezembro</option>
+                        </select>
+
+                        <select name="nascimento-ano" class="nascimento">
+                            <?php
+                                for($i = 1940; $i <= 2021; $i++){
+                            ?>
+                            <option value="<?php echo $i; ?>"><?php echo $i; ?></option>
+                            <?php } ?>
+                        </select>
+                        <div class="clear"></div>
+                    </div><!--w100-->
+
+                    <div class="w100">
+                        <div class="input-radio">
+                            <input type="radio" name="sexo" value="masculino">
+                            <h2>Masculino</h2>
+                        </div><!--radio-->
+                        <div class="input-radio">
+                            <input type="radio" name="sexo" value="feminino">
+                            <h2>Feminino</h2>
+                        </div><!--radio-->
+                        <div class="clear"></div>
+                    </div><!--w100-->
+
+                    <div class="w100">
                         <input type="submit" name="acao" value="Cadastrar!">
                     </div><!--w100-->
 
@@ -67,5 +115,42 @@
             <div class="clear"></div>
         </div><!--center-->
     </section><!--main-->
+
+    <section class="linguas">
+        <div class="center">
+            <a class="selected-lingua" href="#">Português (BR)</a>
+            <a href="#">Português (BR)</a>
+            <a href="#">Português (BR)</a>
+            <a href="#">Português (BR)</a>
+            <a href="#">Português (BR)</a>
+            <a href="#">Português (BR)</a>
+            <a href="#">Português (BR)</a>
+            <a href="#">Português (BR)</a>
+        </div><!--center-->
+
+        <div style="border:0; padding-top: 10px;" class="center">
+            <a href="#">Lorem Ipsum</a>
+            <a href="#">Lorem Ipsum</a>
+            <a href="#">Lorem Ipsum</a>
+            <a href="#">Lorem Ipsum</a>
+            <a href="#">Lorem Ipsum</a>
+            <a href="#">Lorem Ipsum</a>
+            <a href="#">Lorem Ipsum</a>
+            <a href="#">Lorem Ipsum</a>
+            <a href="#">Lorem Ipsum</a>
+            <a href="#">Lorem Ipsum</a>
+            <a href="#">Lorem Ipsum</a>
+            <a href="#">Lorem Ipsum</a>
+            <a href="#">Lorem Ipsum</a>
+            <a href="#">Lorem Ipsum</a>
+            <a href="#">Lorem Ipsum</a>
+            <a href="#">Lorem Ipsum</a>
+            <a href="#">Lorem Ipsum</a>
+            <a href="#">Lorem Ipsum</a>
+            <a href="#">Lorem Ipsum</a>
+            <a href="#">Lorem Ipsum</a>
+            <a href="#">Lorem Ipsum</a>
+        </div><!--center-->
+    </section><!--linguas-->
 </body>
 </html>
